f412f3f95243e7ba8be393849a0c0798be1dcdb4
dmswns0147@naver.com <dmswns0147@naver.com>
2022-11-16 13:30:22 +0900

    second commit

--- src/main/java/com/echoex/ServerApp.java
+++ src/main/java/com/echoex/ServerApp.java
@@ -3,18 +3,23 @@ package com.echoex;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerApp {
-	public static void main(String[] args) {
-		try {
-			ServerSocket ss = new ServerSocket(12345);
-			System.out.println("client connection waiting...");
-			Socket s = ss.accept();
-			System.out.println("client connection success!");
-			Thread t = new Thread(new ServerStateMachine(s));
-			t.start();
-		} catch (IOException e) {
-			e.printStackTrace();
+	
+	private final static int THREAD_CNT = 3;
+	private static ExecutorService threadpool = Executors.newFixedThreadPool(THREAD_CNT);
+	private final static int PORT = 13243;
+
+	public static void main(String[] args) throws IOException {
+		try (ServerSocket listener = new ServerSocket(PORT)) {
+			while (true) {
+				System.out.println("Waiting Connection...");
+				Socket s = listener.accept();
+				threadpool.execute(new ServerStateMachine(s));
+				System.out.println(s.getLocalSocketAddress() + " is Connect");
+			}
 		}
 	}
 }
